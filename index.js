05ad62a1707932b9cb88beab676f4c6e755c088a
Sonia Klimas <sonia.klimas@gmail.com>
2020-06-15 09:00:47 +0200

    add styles and media queries

--- index.js
+++ index.js
@@ -23,52 +23,105 @@ const week = [
     distance: "38m",
     icon: "fas fa-location-arrow",
   },
+  {
+    progressBar: "372",
+    progressNumber: "38",
+  },
+  {
+    minutes: "12",
+  },
+  {
+    currentStreak: "4",
+  },
+  {
+    bestStreak: "12",
+  },
+];
+
+const month = [
+  {
+    number: "1",
+    name: "Marty McFly",
+    distance: "105m",
+    icon: "fas fa-location-arrow",
+  },
+  {
+    number: "2",
+    name: "Walter Wynne",
+    distance: "52m",
+    icon: "fas fa-circle circle",
+  },
+  {
+    number: "3",
+    name: "Annabel Ferdinand ",
+    distance: "50m",
+    icon: "fas fa-location-arrow",
+  },
+  {
+    number: "7",
+    name: "You!",
+    distance: "38m",
+    icon: "fas fa-location-arrow",
+  },
+  {
+    progressBar: "118",
+    progressNumber: "12",
+  },
+  {
+    minutes: "186",
+  },
+  {
+    currentStreak: "7",
+  },
+  {
+    bestStreak: "44",
+  },
 ];
 
 $(document).ready(function () {
-  $(week).each(function (i, el) {
-    const li = document.createElement("li");
-    li.innerHTML = `<span> ${el.number}</span>
-    <span> ${el.name}</span><span> ${el.distance}</span><i class="${el.icon}"> </i>`;
-    //   "<span>" +
-    //   el.number +
-    //   "</span>" +
-    //   "<span>" +
-    //   el.name +
-    //   "<span>" +
-    //   "<span>" +
-    //   el.distance +
-    //   "</span>" +
-    //   "</i class=" +
-    //   el.icon +
-    //   "></i>";
-    $("#listItems")[0].appendChild(li);
+  $('select option[value="week"]').attr("selected", true);
+  let data;
+  const selectHandler = (value) => {
+    switch (value) {
+      case "week":
+        return week;
+
+      case "month":
+        return month;
+
+      default:
+        return week;
+    }
+  };
+
+  const loadData = (data) => {
+    $("#listItems").empty();
+
+    $(data).each(function (i, el) {
+      if (el.number) {
+        const li = document.createElement("li");
+        li.innerHTML = `<span> ${el.number}</span>
+     <span> ${el.name}</span><span> ${el.distance}</span><i class="${el.icon}"> </i>`;
+        $("#listItems")[0].appendChild(li);
+      } else {
+        if (el.progressBar) {
+          $("svg path:last-child").attr(
+            "stroke-dasharray",
+            `${el.progressBar}, 490`
+          );
+          $(".progressBar span").text(el.progressNumber + "m");
+        }
+        $("#minutes").text(el.minutes);
+        $("#currentStreak").text(el.currentStreak);
+        $("#bestStreak").text(el.bestStreak);
+      }
+    });
+  };
+
+  loadData(week);
+
+  $("select").change(function (e) {
+    data = selectHandler(e.target.value);
+    loadData(data);
   });
 });
-
-// const month = [
-//   {
-//     number: "1",
-//     name: "Annabel Ferdinand",
-//     distance: "105m",
-//     icon: "arrow",
-//   },
-//   {
-//     number: "2",
-//     name: "Walter Wynne",
-//     distance: "52m",
-//     icon: "circle",
-//   },
-//   {
-//     number: "3",
-//     name: "Marty McFly",
-//     distance: "50m",
-//     icon: "arrow",
-//   },
-//   {
-//     number: "7",
-//     name: "You!",
-//     distance: "38m",
-//     icon: "arrow",
-//   },
-// ];
